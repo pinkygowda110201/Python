00d9cf7bc64ee0d644e500070eb927d862086919
Priyanka Gowda <71083770+pinkygowda110201@users.noreply.github.com>
2021-09-17 14:34:44 +0530

    Add files via upload

---
+++ p6.py
@@ -0,0 +1,15 @@
+#Printing all positive numbers in the given range
+list1 = [12,-7,5,64,-14]
+y=[]
+print("list1 = ",list1)
+for x in list1 :
+ if x>0 :
+     y.append(x)
+print(y)
+y.clear()
+list2 = [12,14,-95,3]
+print("list2 = ",list2)
+for x in list2 :
+ if x>0 :
+     y.append(x)
+print(y)
